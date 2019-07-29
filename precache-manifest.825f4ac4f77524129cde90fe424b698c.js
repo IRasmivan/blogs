034a3f440c2b7a9b62db65c94789df7bc565255a
CEX <cex@cexs-air-2.mynet>
2019-07-29 20:22:33 +0100

    Updates

---
+++ precache-manifest.825f4ac4f77524129cde90fe424b698c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d11e27aac8e5917ec86bf81a4e62f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f8070f2f21a1998c6cf",
+    "url": "/static/css/2.7e02f3b4.chunk.css"
+  },
+  {
+    "revision": "fcc7eab312ea799b04d1",
+    "url": "/static/css/main.b6118312.chunk.css"
+  },
+  {
+    "revision": "7f8070f2f21a1998c6cf",
+    "url": "/static/js/2.3e9a8863.chunk.js"
+  },
+  {
+    "revision": "fcc7eab312ea799b04d1",
+    "url": "/static/js/main.762729e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
